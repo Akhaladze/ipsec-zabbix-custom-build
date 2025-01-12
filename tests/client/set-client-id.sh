7281bd9108123607ea648ef639b75b9900e4a6d9
Georgiy <4468618@gmail.com>
2025-01-12 08:19:14 +0200

    Update set-client-id.sh

--- tests/client/set-client-id.sh
+++ tests/client/set-client-id.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 mac=$(cat /sys/class/net/eth0/address | tr -d ':')
-#random=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
-#client_id="node-${mac}-${random}"
 client_id="node-${mac}"
 if [ ! -f /etc/ipsec.conf ]; then
     exit 1
 fi
-#sed -i '/^conn myvpn/,/^conn /{s/^leftid=.*//}' /etc/ipsec.conf
-sed -i '/left=%any/a leftid='"${client_id}"'' /etc/ipsec.conf
-
-#ipsec restart
-
-#tail -f /dev/null
+sed -i '/leftid=%any/a\  leftid='"${client_id}"'' /etc/ipsec.conf
